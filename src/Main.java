0cc643941f6ed9132fb5bf81783e63b53f6ebc33
Eric Palmer <muthomiericpalmer@gmail.com>
2023-01-28 18:13:11 +0300

    final changes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         CarLoan loan = new CarLoan(10000, 3, 5, 2000);
         loan.loanValidity();
+        Continents variousContinents = new Continents();
+        variousContinents.myContinents();
     }
 }
